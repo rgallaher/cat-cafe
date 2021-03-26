89ce85f97d0139b7d9629f457013ec312241fb4d
rgallaher <80924048+rgallaher@users.noreply.github.com>
2021-03-25 22:04:33 -0700

    Added monster and food select, inventory, drops

--- js/main.js
+++ js/main.js
@@ -18,6 +18,23 @@ var resources = {
     fishHealth: 10
 }
 
+//food
+
+var food = {
+    name: "Coffee",
+    price: 1,
+    image: "",
+    ingredients: []
+}
+
+var inventory = {
+    eggs: 0,
+    onions: 0,
+    potatoes: 0,
+    meat: 0,
+    essense: 0
+}
+
 //buildings
 
 var barista = {
@@ -45,19 +62,21 @@ var cat = {
 
     //rpg stats
     level: 1,
-    dpsMult: 10,
-    dps: 10,
+    dpsMult: 5,
+    dps: 5,
     health: 100,
     xp: 0
 }
 
 var monster = {
-    health: 100,
-    name: ["Bat", "Dragon", "Worm"],
+    name: "Resting",
     level: 1,
-    stage: 1
+    health: 1,
+    baseHealth: 1,
+    dps: -1,
+    image: "https://64.media.tumblr.com/tumblr_mairyt4v5J1rfjowdo1_500.gif",
+    drops: []
 }
-
 
 //unlocks
 
@@ -67,6 +86,11 @@ var unlocked = {
     tierOne: 0,
     rpgBuy: 0,
     rpg: 0
+}
+
+//capitalize first letter
+function proper(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 //save game to localstorage
@@ -88,6 +112,7 @@ function save() {
 
         //rpg
         monster: monster,
+        inventory: inventory,
 
         //unlocks
         unlocked: unlocked
@@ -127,6 +152,9 @@ function loadSave() {
         if (typeof savegame.monster !== undefined) {
         monster = savegame.monster
         }
+        if (typeof savegame.inventory !== undefined) {
+            monster = savegame.inventory
+        }
 
         //unlocks
         if (typeof savegame.unlocked !== undefined) {
@@ -148,9 +176,24 @@ function coffeeClick(number) {
 }
 
 function sellClick(number) {
-    if(number > 0 && resources.coffees >= number) {
-        resources.coffees -= number
-        resources.money += (number * (resources.coffeePrice + cat.tipBonus * cat.count))
+    if(number > 0) {
+        if(food.name == "Coffee") {
+            resources.money += (number * (food.price + cat.tipBonus * cat.level))
+        }
+        else {
+            var ingredientCheck = 1
+            for(var i = 0; i < food.ingredients.length; i++) {
+                if(inventory[food.ingredients[i]] < 1) {
+                    ingredientCheck = 0
+                }
+            }
+            if(ingredientCheck == 1) {
+                for(var i = 0; i < food.ingredients.length; i++) {
+                    inventory[food.ingredients[i]] -= 1
+                }
+                resources.money += (number * (food.price + cat.tipBonus * cat.level))
+            }
+        }
         gameTick()
     }
 }
@@ -162,6 +205,67 @@ function fishClick(number) {
         gameTick()
     }
 }
+
+//food select function
+
+var startup = 0
+
+function foodSelect(number) {
+    if(number == 0) {
+        food.name = "Coffee"
+        food.price = 1
+        food.image = "https://i.imgur.com/TW6XbcN.png"
+        food.ingredients.length = 0
+    }
+//ADD ADDITIONAL FOODS HERE
+    if(number == 1) {
+        food.name = "Omelette"
+        food.price = 10
+        food.image = "https://i.imgur.com/TW6XbcN.png"
+        food.ingredients.length = 0
+        food.ingredients.push("eggs")
+    }
+    if(number == 2) {
+        food.name = "Scramble"
+        food.price = 30
+        food.image = "https://i.imgur.com/TW6XbcN.png"
+        food.ingredients.length = 0
+        food.ingredients.push("eggs")
+        food.ingredients.push("onions")
+    }
+    if(number == 3) {
+        food.name = "French Fries"
+        food.price = 150
+        food.image = "https://i.imgur.com/TW6XbcN.png"
+        food.ingredients.length = 0
+        food.ingredients.push("potatoes")
+    }
+    if(number == 4) {
+        food.name = "Steak"
+        food.price = 1000
+        food.image = "https://i.imgur.com/TW6XbcN.png"
+        food.ingredients.length = 0
+        food.ingredients.push("meat")
+    }
+
+    //food ui update
+    document.getElementById("foodPrice").innerHTML = "$" + food.price
+
+    var ingredients = ""
+    if(food.name !== "Coffee") {
+        for(var i = 0; i < food.ingredients.length; i++) {
+            if(i > 0) {
+                ingredients += ", "
+            }
+            ingredients += proper(food.ingredients[i])
+        }
+    }
+    else {
+        ingredients = "None"
+    }
+    document.getElementById("ingredients").innerHTML = ingredients
+}
+
 
 //building buy functions
 
@@ -247,26 +351,108 @@ function catHunger() {
 
 //rpg
 
+function monsterSelect(number) {
+    if(number == 0) {
+        monster.name = "Resting"
+        monster.level = 0,
+        monster.health = 1
+        monster.baseHealth = 1
+        monster.dps = -1
+        monster.image = "https://64.media.tumblr.com/tumblr_mairyt4v5J1rfjowdo1_500.gif"
+        monster.drops.length = 0
+    }
+//ADD ADDITIONAL MONSTERS HERE
+    if(number == 1) {
+        monster.name = "Eggdog"
+        monster.level = 1
+        monster.health = 50
+        monster.baseHealth = 50
+        monster.dps = 1
+        monster.image = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/16b36037-0122-4e7e-98c9-2f23fce5ec48/ddonnuq-241dd4b8-e95f-48c9-bf21-61353a46bc07.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMTZiMzYwMzctMDEyMi00ZTdlLTk4YzktMmYyM2ZjZTVlYzQ4XC9kZG9ubnVxLTI0MWRkNGI4LWU5NWYtNDhjOS1iZjIxLTYxMzUzYTQ2YmMwNy5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.lkTCFLSU2lLMgEEJPNnnTNN9Go9QHOrMpRiKLw6YUcg"
+        monster.drops.length = 0
+        monster.drops.push("eggs")
+    }
+    if(number == 2) {
+        monster.name = "Onyon"
+        monster.level = 5
+        monster.health = 300
+        monster.baseHealth = 300
+        monster.dps = 6
+        monster.image = "https://i2.wp.com/oniongames.jp/onion_wp/wp-content/uploads/2014/02/onionblog0219.gif?ssl=1"
+        monster.drops.length = 0
+        monster.drops.push("onions")
+    }
+    if(number == 3) {
+        monster.name = "Tato"
+        monster.level = 15
+        monster.health = 750
+        monster.baseHealth = 750
+        monster.dps = 15
+        monster.image = "https://64.media.tumblr.com/5dc5944477d4815685c1c93f7dbc47cb/tumblr_n2bewhurSE1sr05r6o1_500.gif"
+        monster.drops.length = 0
+        monster.drops.push("potatoes")
+    }
+    if(number == 4) {
+        monster.name = "Pig God"
+        monster.level = 75
+        monster.health = 4800
+        monster.baseHealth = 4800
+        monster.dps = 70
+        monster.image = "https://i.giphy.com/media/jy7mx8ZRjnDcnZZSd7/giphy.webp"
+        monster.drops.length = 0
+        monster.drops.push("meat")
+        monster.drops.push("essence")
+    }
+
+    //update ui
+    document.getElementById("monsterName").innerHTML = monster.name
+    document.getElementById("monsterImage").src = monster.image
+    if(number == 0) {
+        document.getElementById("monsterLevel").style.display = "none"
+        document.getElementById("monsterHealthBar").style.display = "none"
+        document.getElementById("monsterDropsContainer").style.display = "none"
+    }
+    else {
+        document.getElementById("monsterLevel").style.display = "table-row"
+        document.getElementById("monsterHealthBar").style.display = "table-row"
+        document.getElementById("monsterDropsContainer").style.display = "table-cell"
+        document.getElementById("monsterLevel").innerHTML = "Level " + monster.level
+        
+        var drops = ""
+        for(var i = 0; i < monster.drops.length; i++) {
+            if(i > 0) {
+                drops += "<br>"
+            }
+            drops += proper(monster.drops[i])
+        }
+
+        document.getElementById("monsterDrops").innerHTML = drops
+    }
+}
+
 function combat(number) {
     if(unlocked.rpg == 1) {
-        monster.health -= (cat.dps / number)
-        cat.health -= (monster.level / number)
+        if(monster.name !== "Resting") {
+            monster.health -= (cat.dps / number)
+        }
+        if(cat.health - monster.dps / number <= 100) {
+            cat.health -= (monster.dps / number)
+        }
         if(monster.health <= 0) {
-            monster.stage += 1
-            monster.level = Math.ceil(monster.stage / 10)
-            monster.health = 100 * monster.level
+            monster.health = monster.baseHealth
             cat.xp += monster.level
             if (cat.xp >= cat.level * 10) {
                 cat.xp = 0
                 cat.level += 1
             }
             cat.dps = cat.dpsMult * cat.level
+            var drop = monster.drops[Math.floor(Math.random() * monster.drops.length)]
+            inventory[drop] += 1
         }
         if(cat.health <= 0) {
-            monster.stage = 1
-            monster.level = Math.ceil(monster.stage / 10)
-            monster.health = 100 * monster.level
-            cat.health = 100 * cat.level
+            cat.health = 0
+            monsterSelect(0)
+            document.getElementById("monsterSelect").value = "0"
         }
     }
 }
@@ -288,6 +474,8 @@ function upgrades(string) {
     if(string == "outsideCat" && resources.money >= 3000) {
         unlocked.rpg = 1
         resources.money -= 3000
+        monsterSelect(0)
+        document.getElementById("monsterSelect").value = "0"
         document.getElementById("outsideCat").style.display = "none"
         document.getElementById("rpgUnlock").style.display = "block"
         document.getElementById("rpgStats").style.display = "block"
@@ -337,6 +525,12 @@ function gameTick() {
 
     document.getElementById("money").innerHTML = "$" + Math.floor(resources.money)
     document.getElementById("coffees").innerHTML = Math.floor(resources.coffees)
+
+    //set food to coffee on startup
+    if(startup == 0) {
+        document.getElementById("foodSelect").value = "0"
+        startup = 1
+    }
 
     //disable purchase buttons when too expensive
 
@@ -395,7 +589,7 @@ function gameTick() {
 
     if(unlocked.hunger == 1) {
         document.getElementById("catHunger").innerHTML = cat.hungerText
-        document.getElementById("feedCat").innerHTML = "Feed Cats (Cost: " + cat.count + " Fish)"
+        document.getElementById("feedCat").innerHTML = "Feed (Cost: " + cat.count + " Fish)"
         if(resources.fish >= cat.count && cat.hunger + resources.fishHunger < 100) {
             document.getElementById("feedCat").setAttribute('class','nes-btn')
         }
@@ -429,13 +623,21 @@ function gameTick() {
 
     if(unlocked.rpg == 1) {
         document.getElementById("catLevel").innerHTML = "Level " + cat.level
+
         document.getElementById("catXp").setAttribute('value', cat.xp * 10 / cat.level)
+        document.getElementById("catXpText").innerHTML = cat.xp + "/" + cat.level * 10
+
         document.getElementById("catHealth").setAttribute('value', cat.health / cat.level)
+        document.getElementById("catHealthText").innerHTML = cat.health + "/" + cat.level * 100
+
         document.getElementById("catDps").innerHTML = "DPS: " + cat.dps
-        document.getElementById("stage").innerHTML = "Stage " + monster.stage
-        document.getElementById("monsterLevel").innerHTML = "Level " + monster.level
-        document.getElementById("monsterHealth").setAttribute('value', monster.health / monster.level)
+
+        document.getElementById("monsterHealth").setAttribute('value', monster.health / monster.baseHealth * 100)
+        document.getElementById("monsterHealthText").innerHTML = monster.health + "/" + monster.baseHealth
     }
+
+    //inventory ui update
+    document.getElementById("eggs").innerHTML = inventory.eggs
 
     //upgrades ui update
 
